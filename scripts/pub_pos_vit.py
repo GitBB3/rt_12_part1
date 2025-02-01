e2a12c22839f490d62e6f3b690f2841eff6354fe
GitBB3 <beaujean.bertille@orange.fr>
2025-02-01 18:47:55 +0000

    action client

--- scripts/pub_pos_vit.py
+++ scripts/pub_pos_vit.py
@@ -1,50 +1,36 @@
 #!/usr/bin/env python
 
 import rospy
+import actionlib
 from nav_msgs.msg import Odometry
-#from geometry_msgs.msg import Point, Twist
-from assignment_2_2024.msg import RobotOdom
+from assignment_2_2024.msg import RobotOdom, PlanningAction, PlanningGoal
+from actionlib_msgs.msg import GoalStatus
 
-#position_msg = Point()
-#velocity_msg = Twist()
 robot_state = RobotOdom()
 
 def odom_callback(msg):
-    #global position_msg, velocity_msg
-    global robot_state
-    
-    #position_msg.x = msg.pose.pose.position.x
-    #position_msg.y = msg.pose.pose.position.y
-    #position_msg.z = 0
-    
-    robot_state.x = msg.pose.pose.position.x
-    robot_state.y = msg.pose.pose.position.y
-    robot_state.z = 0
 
-    #velocity_msg.linear.x = msg.twist.twist.linear.x
-    #velocity_msg.angular.z = msg.twist.twist.angular.z
-    
-    robot_state.vel_x = msg.twist.twist.linear.x
-    robot_state.vel_z = msg.twist.twist.angular.z
+	robot_state.x = msg.pose.pose.position.x
+	robot_state.y = msg.pose.pose.position.y
+	robot_state.z = 0
+
+	robot_state.vel_x = msg.twist.twist.linear.x
+	robot_state.vel_z = msg.twist.twist.angular.z
 
 
 def publish_data(event):
-    #position_pub.publish(position_msg)
-    #velocity_pub.publish(velocity_msg)
-    robot_state_pub.publish(robot_state)
+	robot_state_pub.publish(robot_state)
     
-    rospy.loginfo(f"Position: x={robot_state.x}, y={robot_state.y}")
-    rospy.loginfo(f"Velocity: Vx={robot_state.vel_x}, Wz={robot_state.vel_z}")
+	rospy.loginfo(f"Position: x={robot_state.x}, y={robot_state.y}")
+	rospy.loginfo(f"Velocity: Vx={robot_state.vel_x}, Wz={robot_state.vel_z}")
 
 if __name__ == '__main__':
-    rospy.init_node('odom_publisher', anonymous=True)
-
-    #position_pub = rospy.Publisher('/robot_position', Point, queue_size=10)
-    #velocity_pub = rospy.Publisher('/robot_velocity', Twist, queue_size=10)
-    robot_state_pub = rospy.Publisher('/robot_odom', RobotOdom, queue_size=10)
+	rospy.init_node('odom_publisher', anonymous=True)
     
-    rospy.Subscriber('/odom', Odometry, odom_callback)
+	robot_state_pub = rospy.Publisher('/robot_odom', RobotOdom, queue_size=10)
     
-    rospy.Timer(rospy.Duration(1.0), publish_data)
+	rospy.Subscriber('/odom', Odometry, odom_callback)
     
-    rospy.spin()
+	rospy.Timer(rospy.Duration(1.0), publish_data)	
+	
+	rospy.spin()
--- scripts/set_target.py
+++ scripts/set_target.py
@@ -2,12 +2,55 @@
 
 import rospy
 import actionlib
-from assignment_2_2024.msg import Planning.action
+from nav_msgs.msg import Odometry
+from assignment_2_2024.msg import RobotOdom, PlanningAction, PlanningGoal
+from actionlib_msgs.msg import GoalStatus
+
+def set_target_client():
+	global first_start
+	client = actionlib.SimpleActionClient('/reaching_goal', PlanningAction)
+	client.wait_for_server()
+	
+	while not rospy.is_shutdown():
+		rospy.loginfo("Press T to set a new target. Press C to cancel the previous target.")
+		act = input()
+		
+		goal = PlanningGoal()
+		goal.target_pose.pose.position.x = rospy.get_param('/des_pos_x', 0.0)
+		goal.target_pose.pose.position.y = rospy.get_param('/des_pos_y', 1.0)
+		
+		if act == "t" or act == "T":
+			rospy.loginfo("Set the target coordinates (x,y)")
+			x_tar = float(input("Type the desired x coordinate : "))
+			y_tar = float(input("Type the desired y coordinate : "))
+			
+			rospy.set_param('/des_pos_x', x_tar)
+			rospy.set_param('/des_pos_y', y_tar)
+			
+			goal.target_pose.pose.position.x = x_tar
+			goal.target_pose.pose.position.y = y_tar
+			client.send_goal(goal)
+			
+			rospy.loginfo(f"New target has been set : x={goal.target_pose.pose.position.x}, y={goal.target_pose.pose.position.y}")
+		    	
+
+		elif act == "c" or act == "C":
+			if client.get_state() == GoalStatus.ACTIVE:
+				rospy.loginfo(f"Cancelling target (x={goal.target_pose.pose.position.x}, y={goal.target_pose.pose.position.y})...")
+				client.cancel_goal()
+				rospy.loginfo("Target cancelled.")
+			else:
+				rospy.loginfo("The robot is not currently aiming at a target.")
+
+		if client.get_state() == GoalStatus.SUCCEEDED:
+			rospy.loginfo(f"Target (x={goal.target_pose.pose.position.x}, y={goal.target_pose.pose.position.y}) reached.")	
+		
+		
 
 
-
-
-def main():
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+	rospy.init_node('action_client', anonymous=True)
+	
+	set_target_client()
+	
+	rospy.spin()
