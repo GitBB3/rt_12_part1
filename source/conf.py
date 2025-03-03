b49b73f83a1bb65d53a46ad4801eaf821e935de9
GitBB3 <beaujean.bertille@orange.fr>
2025-03-03 12:50:39 +0100

    Quickstart Sphinx.

---
+++ source/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import subprocess
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+show_authors=True
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'assignment_2_2024'
+copyright = '2025, Bertille Beaujean beaujean.bertille@orange.fr'
+author = 'Bertille Beaujean beaujean.bertille@orange.fr'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.doctest',
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.todo',
+	'sphinx.ext.coverage',
+	'sphinx.ext.mathjax',
+	'sphinx.ext.ifconfig',
+	'sphinx.ext.viewcode',
+	'sphinx.ext.githubpages',
+	'sphinx.ext.napoleon',
+	'sphinx.ext.inheritance_diagram',
+	'breathe'
+	]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+highlight_language = 'python'
+source_suffix = '.rst'
+master_doc = 'index'
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,31 @@
+.. assignment_2_2024 documentation master file, created by
+   sphinx-quickstart on Mon Mar  3 12:27:38 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to assignment_2_2024's documentation!
+=============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+Assignment 2 documentation
+***************************
+
+This is the documentation of the 2nd assignment or Research Track 1 realised during the first semester.
+
+Test 1
+=======
+
+..automodule:: scripts.set_target
+  :members:
